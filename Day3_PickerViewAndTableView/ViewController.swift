359c9063fc386301256832f447520012f64e0a69
Komal2006 <komaldeepkr99@gmail.com>
2019-10-29 19:08:32 -0400

    table view

--- Day3_PickerViewAndTableView/ViewController.swift
+++ Day3_PickerViewAndTableView/ViewController.swift
@@ -18,8 +18,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
 
     @IBOutlet weak var pickerCountries: UIPickerView!
     let countries = ["India","Canada", "United States Of America", "Russia", "Sri Lanka", "China", "Pakistan", "Mexico"]
-    
-    
+//    var imageArray: [UIImage] = [UIImage(named: "ad")!,
+//    UIImage(named: "ae")!,  UIImage(named: "af")!,
+//    UIImage(named: "ag")!,  UIImage(named: "al")!,
+//    UIImage(named: "am")!,  UIImage(named: "ao")!,
+//     UIImage(named: "ar")!,  UIImage(named: "at")!]
     
    
     
@@ -38,20 +41,27 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return self.countries.count
     }
     
-    func pickerView(_pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
     {
         print("TITLE")
         return self.countries[row]
     }
-    func pickerView(_pickerView: UIPickerView, didSelectRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView
+    private func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, forComponent component: Int, reusing view: UIView?)
     {
-        
-        let myview = UIView(frame: CGRect(x: 0, y: 0, width: pickerView.bounds.width - 30, height: 60))
+        print(self.countries[row])
+//        let myview = UIView(frame: CGRect(x: 0, y: 0, width: pickerView.bounds.width - 30, height: 60))
     }
-    func pickerView(_ pickerView: UIPickerView, viewForRow component: Int) -> CGFloat {
-        <#code#>
-    }
-    
+//    func pickerView(_ pickerView: UIPickerView, viewForRow component: Int) -> CGFloat {
+//        <#code#>
+//    }
+//    func pickerView(_pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+//    {
+//        if component == 0
+//        {
+//            return Array(self.countries.keys)[row]
+//        }
+//         return Array(self.countries.keys)[row]
+//    }
 
 
 }
